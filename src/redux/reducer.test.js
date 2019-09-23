84a6b2d1b6dcad19c7f6b267199825dc3c0e2911
Mateusz <mateusz@qunabu.com>
2019-09-23 08:58:15 +0200

    tests

--- src/redux/reducer.test.js
+++ src/redux/reducer.test.js
@@ -42,49 +42,4 @@ describe("todos reducer", () => {
       albums: outputData
     });
   });
-
-  /*
-
-  it('should handle ADD_TODO', () => {
-    expect(
-      reducer([], {
-        type: types.ADD_TODO,
-        text: 'Run the tests'
-      })
-    ).toEqual([
-      {
-        text: 'Run the tests',
-        completed: false,
-        id: 0
-      }
-    ])
-
-    expect(
-      reducer(
-        [
-          {
-            text: 'Use Redux',
-            completed: false,
-            id: 0
-          }
-        ],
-        {
-          type: types.ADD_TODO,
-          text: 'Run the tests'
-        }
-      )
-    ).toEqual([
-      {
-        text: 'Run the tests',
-        completed: false,
-        id: 1
-      },
-      {
-        text: 'Use Redux',
-        completed: false,
-        id: 0
-      }
-    ])
-  })
-  */
 });
